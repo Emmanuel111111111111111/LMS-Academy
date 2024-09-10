ee781ecba167b1ad17a11e85cd7c88a2f1117103
Emmanuel111111111111111 <uchezeemanuel@gmail.com>
2024-09-10 11:03:51 +0100

    Login backend

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,77 +1,78 @@
 import React, { useState } from "react";
+
 import { getImageUrl } from "../../utilis";
 import styles from "./Login.module.css";
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+import axios from 'axios'
+import { useNavigate } from "react-router-dom";
 
 
 export const Login = () => {
 
+  const [ email, setEmail ] = useState('');
+  const [ password, setPassword ] = useState('');
+  const [ response, setResponse ] = useState('');
+  const [ errorMessage, setErrorMesage ] = useState(false);
   const navigate = useNavigate();
 
-  const [ values, setValues ] = useState({
-    email: '',
-    phone_number: '',
-    password: ''
-  })
+ 
 
-  console.log(values)
-
-  const handleInput = (event) => {
-    setValues(prev => ({...prev, [event.target.name] : [event.target.value]}))
-  }
-
-  const handleSubmit = (event) => {
+  function handleSubmit(event) {
     event.preventDefault();
-    axios.post('http://localhost:8081/login', values)
-    .then(res => console.log(res))
-    .catch(err => console.log(err));
-    navigate('/dashboard');
+    axios.post('http://localhost:8081/login', {email, password})
+      .then(res => {
+        console.log(res)
+        if (res.data === "No records") setErrorMesage(true);
+        else if (res.data === "Login Successfully") navigate('/dashboard');
+      })
+      .catch(err => console.log(err));   
   }
 
-  const [ showPassword, setShowPassword ] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
 
-    return(
-        <div className={styles.big}>
-            <div className={styles.bread}>
-            <img src={getImageUrl("Frame 349.png")} alt="" />
-                <h3>The ultimate financial management solution. Seize control,gain insightful data.</h3>
-            </div>
-              <div className={styles.crumb}>
-                <div className={styles.pan}>
-                      <a href="/CWG"> <img src={getImageUrl("arrow.png")} alt="" /></a>
-                      <h5><span>Back to</span> Home</h5>
-                </div>
-              <div className={styles.crumbs}>
-                <h1>Log in to your account</h1>
-                <p>Lets help you get started on CWG Academy</p>
-              </div>
-             <div className={styles.forms}>
-              <form onSubmit={handleSubmit}>
-                <div className={styles.formgroup}>
-                     <label for="name">Phone Number or Email Address</label>
-                    <input placeholder="Enter your phone number or email address" name="email" onChange={handleInput} />
-                </div>
-                <div className={styles.formgroup}>
-                    <label for="Password">Password</label>
-                    <div className={styles.password}>
-                      
-                      <input type={showPassword ? 'text' : 'password'} placeholder="Enter your password" name="password" onChange={handleInput} />
-                      <button type="button" onClick={() => setShowPassword((showPassword) => !showPassword)}><img src={getImageUrl("off.png")} alt="y" /></button>
+  return (
+    <div className={styles.big}>
+      
+      <div className={styles.bread}>
+        <img src={getImageUrl("Frame 349.png")} alt="" />
+        <h3>The ultimate financial management solution. Seize control,gain insightful data.</h3>
+      </div>
 
-                    </div>
-                </div>
-                <p>Forgot password?<a href="/Reset"> <span>Reset Password</span> </a></p>
-
-                <div className={styles.home}>
-                  <a href="/Home"><button className={styles.butt} type="submit">Log In</button></a>
-                  <p>Don't Have An Account? <a href="/Account">Create An Account</a></p>
-                </div>
-              </form>
-             </div>
-             
-            </div>
-
+      <div className={styles.crumb}>
+        <div className={styles.pan}>
+          <a href="/CWG"><img src={getImageUrl("arrow.png")} alt="" /></a>
+          <h5><span>Back to</span> Home</h5>
         </div>
-    )
+        <div className={styles.crumbs}>
+          <h1>Log in to your account</h1>
+          <p>Lets help you get started on CWG Academy</p>
+        </div>
+        <div className={styles.forms}>
+          <form onSubmit={handleSubmit}>
+            {errorMessage && <p style={{color: 'red'}}>Wrong username or password. Try again</p>}
+            <div className={styles.formgroup}>
+              <label for="name">Phone Number or Email Address</label>
+              <input placeholder="Enter your phone number or email address" name="email" onChange={e => setEmail(e.target.value)} />
+            </div>
+            <div className={styles.formgroup}>
+              <label for="Password">Password</label>
+              <div className={styles.password}>
+
+                <input type={showPassword ? 'text' : 'password'} placeholder="Enter your password" name="password" onChange={e => setPassword(e.target.value)} />
+                <button type="button" onClick={() => setShowPassword((showPassword) => !showPassword)}><img src={getImageUrl("off.png")} alt="y" /></button>
+
+              </div>
+            </div>
+            <p>Forgot password?<a href="/Reset"> <span>Reset Password</span> </a></p>
+
+            <div className={styles.home}>
+              <button className={styles.butt} type="submit">Log In</button>
+              <p>Don't Have An Account? <a href="/Account">Create An Account</a></p>
+            </div>
+          </form>
+        </div>
+
+      </div>
+
+    </div>
+  )
 }
